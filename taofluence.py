eb3bf5f45ca5ff449a898d7c2dd2a19668d3b133
Ed <ed@ed-VirtualBox.(none)>
2012-11-07 18:35:12 +1100

    First commit Committer: Edwin Dawson <edwin.dawson@gmail.com> 	new file:   taofluence.py

---
+++ taofluence.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from requests.auth import HTTPBasicAuth
+import sys
+
+#Checking that all required arguments (username and password) were passed.
+
+if len(sys.argv) < 3:
+    argumentsnotset = "\nError: one or more arguments were not passed. \n\nUsage is like so: \n\npython zen-create-post.py ZENDESKUSERNAME ZENDESKPASSWORD\n" 
+    print argumentsnotset
+    sys.exit(1)
+
+username = sys.argv[1]
+password = sys.argv[2]
+
+auth = HTTPBasicAuth(username,password)
+
+url = 'https://ninefold.zendesk.com/api/v2/topics.json'
+
+payload = {"topic": {"forum_id": 21371187, "title": "Test Announcement Topic from REST", "body": "This is a test created via REST API."}}
+
+data=json.dumps(payload)
+
+headers = {'content-type': 'application/json'}
+
+r = requests.post(url=url, auth=auth, data=data, headers=headers)
+print r.text
+print r.status_code
+print r.headers
+print r.headers['content-type']
