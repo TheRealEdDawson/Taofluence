3d8ba3cae7249cae1bf5b2653a9bbef78eef4bbd
Ed <ed@ed-VirtualBox.(none)>
2012-11-08 17:04:38 +1100

    Committer: Ed <edwin.dawson@gmail.com> modified:   taofluence.py

--- taofluence.py
+++ taofluence.py
@@ -2,24 +2,41 @@ import requests
 import json
 from requests.auth import HTTPBasicAuth
 import sys
+import string
 
 #Checking that all required arguments (username and password) were passed.
 
-if len(sys.argv) < 3:
-    argumentsnotset = "\nError: one or more arguments were not passed. \n\nUsage is like so: \n\npython zen-create-post.py ZENDESKUSERNAME ZENDESKPASSWORD\n" 
+if len(sys.argv) < 4:
+    argumentsnotset = "\nError: one or more arguments were not passed. \n\nUsage is like so: \n\npython zen-create-post.py ZENDESKUSERNAME ZENDESKPASSWORD FORUMID"
     print argumentsnotset
     sys.exit(1)
 
 username = sys.argv[1]
 password = sys.argv[2]
+forum_id = sys.argv[3]
 
 auth = HTTPBasicAuth(username,password)
 
 url = 'https://ninefold.zendesk.com/api/v2/topics.json'
 
-payload = {"topic": {"forum_id": 21371187, "title": "Test Announcement Topic from REST", "body": "This is a test created via REST API."}}
+title_text = "Test Topic from REST"
+body_text = "This is a test created via REST API."
+jsonbit1 = '''{"topic": {"forum_id": '''
+jsonbit2 = ''', "title": \"'''
+jsonbit3 = '''\" , "body": \"''' 
+jsonbit4 = '''\"}}'''
 
-data=json.dumps(payload)
+payload = jsonbit1 + forum_id + jsonbit2 + title_text + jsonbit3 + body_text + jsonbit4
+
+print payload #temporary debug point
+#sys.exit(1) #temporary breakpoint
+
+#data=json.dumps(payload)
+#data=(str.replace(data, '\\', ''))
+data=payload
+
+print data , "\n" #temporary debug point
+#sys.exit(1) #temporary breakpoint
 
 headers = {'content-type': 'application/json'}
 
