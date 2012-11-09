36e5e48236b954caa44fe5edaf20d91fc2a6104f
Ed <ed@ed-VirtualBox.(none)>
2012-11-09 16:45:16 +1100

    Added code to connect to  a Confluence instance and retrieve a list of pages in a space.  Committer: Ed <ed@edwin.dawson@gmail.com> 	modified:   taofluence.py

--- taofluence.py
+++ taofluence.py
@@ -4,33 +4,51 @@ from requests.auth import HTTPBasicAuth
 import sys
 import string
 import datetime
+from xmlrpclib import ServerProxy as Server
 
-#Checking that all required arguments (username and password) were passed.
+#Checking that all required command-line arguments (username and password) were passed.
 
-if len(sys.argv) < 4:
-    argumentsnotset = "\nError: one or more arguments were not passed. \n\nUsage is like so: \n\npython zen-create-post.py ZENDESKUSERNAME ZENDESKPASSWORD FORUMID"
+if len(sys.argv) < 8:
+    argumentsnotset = "\nError: one or more arguments were not passed. \n\nUsage is like so: \
+    \n\npython zen-create-post.py ZENDESKURL ZENDESKUSERNAME ZENDESKPASSWORD ZENDESKFORUMID \
+    CONFLUENCEURL CONFLUENCEUSERNAME CONFLUENCEPASSWORD CONFLUENCESPACE"
     print argumentsnotset
     sys.exit(1)
 
-zendesk_username = sys.argv[1]
-zendesk_password = sys.argv[2]
-zendesk_forum_id = sys.argv[3]
-zendesk_url = sys.argv[4]
-# The REST access point for creating topics in Zendesk is 'https://ninefold.zendesk.com/api/v2/topics.json'
+zendesk_url = sys.argv[1]
+# The REST access point for creating topics in Zendesk is 'https://YOURDOMAIN.zendesk.com/api/v2/topics.json'
+zendesk_username = sys.argv[2]
+zendesk_password = sys.argv[3]
+zendesk_forum_id = sys.argv[4]
 
-#Set up login credentials from command-line arguments
+# Setting Confluence URL, Space, username and password. Note confluence_server requires a https URL.
+confluence_server_url = sys.argv[5]
+conf_user = sys.argv[6]
+conf_pwd = sys.argv[7]
+conf_space = sys.argv[8]
+
+# Access the Confluence site via XML-RPC to retrieve the list of all pages in the space (once-off)
+s = Server(confluence_server_url + "/rpc/xmlrpc")
+s = s.confluence1
+confluence_token = s.login(conf_user,conf_pwd)
+# this is a "dictionary" (spaceindex)
+confluence_spaceindex = s.getPages(confluence_token, conf_space)
+print confluence_spaceindex #temporary debug point
+s.logout(confluence_token)
+
+#Set up Zendesk login credentials from command-line arguments
 zendesk_auth = HTTPBasicAuth(zendesk_username,zendesk_password)
 
-#define dymanic content to go into the page
+#define dynamic content to go into the Zendesk page
 time_now =str(datetime.datetime.now())
 zendesk_title_text = "Test Topic from REST " + time_now
 zendesk_body_text = "This is a test created via REST API at " + time_now
 
+#build the JSON request that will be passed via REST
 jsonbit1 = '''{"topic": {"forum_id": '''
 jsonbit2 = ''', "title": \"'''
 jsonbit3 = '''\" , "body": \"''' 
 jsonbit4 = '''\"}}'''
-
 payload = jsonbit1 + zendesk_forum_id + jsonbit2 + zendesk_title_text + jsonbit3 + zendesk_body_text + jsonbit4
 zendesk_data=payload
 
