f1e3d79d44bc8b62eaa98d27ac27dab351d5e991
Ed <ed@ed-VirtualBox.(none)>
2012-11-09 18:15:33 +1100

    Added some new commands to retrieve page content, which need to be migrated to version 2 of the Atlassian remote API. Committer: Ed <edwin.dawson@gmail.com> 	modified:   taofluence.py

--- taofluence.py
+++ taofluence.py
@@ -15,13 +15,14 @@ if len(sys.argv) < 8:
     print argumentsnotset
     sys.exit(1)
 
+# Setting up Zendesk server details. The zendesk_url looks like 'https://YOURDOMAIN.zendesk.com'
 zendesk_url = sys.argv[1]
-# The REST access point for creating topics in Zendesk is 'https://YOURDOMAIN.zendesk.com/api/v2/topics.json'
+zendesk_url = (zendesk_url + "/api/v2/topics.json")
 zendesk_username = sys.argv[2]
 zendesk_password = sys.argv[3]
 zendesk_forum_id = sys.argv[4]
 
-# Setting Confluence URL, Space, username and password. Note confluence_server requires a https URL.
+# Setting Confluence server details. The confluence_server_url looks like https://YOURCONFLUENCEDOMAIN.com".
 confluence_server_url = sys.argv[5]
 conf_user = sys.argv[6]
 conf_pwd = sys.argv[7]
@@ -34,24 +35,26 @@ confluence_token = s.login(conf_user,conf_pwd)
 # this is a "dictionary" (spaceindex)
 confluence_spaceindex = s.getPages(confluence_token, conf_space)
 #print confluence_spaceindex #temporary debug point
-s.logout(confluence_token)
+#s.logout(confluence_token) 
 
 #Set up Zendesk login credentials from command-line arguments
 zendesk_auth = HTTPBasicAuth(zendesk_username,zendesk_password)
 
 #Loop to create new pages in Zendesk from the list of pages in Confluence.
-
 for page_summary in confluence_spaceindex:
+    confluence_content_space = page_summary['space']
     confluence_content_url = page_summary['url']
     confluence_content_title = page_summary['title']
     confluence_content_version = page_summary['version']
     confluence_content_parent = page_summary['parentId']
     confluence_content_id = page_summary['id']
     confluence_content_permissions = page_summary['permissions']
+    #Retrieve the page content for the page in context
+    #confluence_content_pagecontent = s.confluence2.getPage(confluence_token, confluence_content_space, confluence_content_title)
     #define dynamic content to go into the Zendesk page
     time_now =str(datetime.datetime.now())
     zendesk_title_text = confluence_content_title + " " + time_now
-    zendesk_body_text = time_now + " " + confluence_content_id + " " + confluence_content_version + " " + confluence_content_parent + " " + confluence_content_permissions + " " + confluence_content_url
+    zendesk_body_text = time_now + " " + confluence_content_space + " " + confluence_content_id + " " + confluence_content_version + " " + confluence_content_parent + " " + confluence_content_permissions + " " + confluence_content_url #+ " " + confluence_content_pagecontent
     #build the JSON request that will be passed via REST
     jsonbit1 = '''{"topic": {"forum_id": '''
     jsonbit2 = ''', "title": \"'''
@@ -69,4 +72,4 @@ for page_summary in confluence_spaceindex:
     print r.status_code #temporary debug point
     print r.headers #temporary debug point
     print r.headers['content-type'] #temporary debug point
-    #sys.exit(1) #temporary breakpoint
+    sys.exit(1) #temporary breakpoint
